c3cec2fb53c363e2aa18d71420f5c5c89dfc9689
Michał Kozak <kozak_m@outlook.com>
2018-09-02 22:31:28 +0200

    Update script.js

--- menu/script.js
+++ menu/script.js
@@ -1,1 +1,17 @@
-...
+$(document).ready(function () {
+    $("ul li:nth-child(1)").hover(function () {
+        $("#line").css("margin-left","3px")
+    })
+    $("ul li:nth-child(2)").hover(function () {
+        $("#line").css("margin-left", "113px")
+    })
+    $("ul li:nth-child(3)").hover(function () {
+        $("#line").css("margin-left", "223px")
+    })
+    $("ul li:nth-child(4)").hover(function () {
+        $("#line").css("margin-left", "333px")
+    })
+    $("ul li:nth-child(5)").hover(function () {
+        $("#line").css("margin-left", "443px")
+    })
+})
