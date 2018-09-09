491cd35a9997a6e095e69a64335b9f38a2991848
Michał Kozak <kozak_m@outlook.com>
2018-09-09 08:22:06 +0200

    Update script.js

--- animation/script.js
+++ animation/script.js
@@ -3,3 +3,12 @@ var objectTwo=$('#objectTwo');
 var objectThree=$('#objectThree');
 
 var layer=$('#layer');
+
+layer.mousemove(function(e){
+    var valueX=(e.pageX * -1 / 12);
+    var valueY=(e.pageY * -1 / 12);
+
+    objectOne.css({
+        'transform':'translate3d('+valueX+'px,'+valueY+'px,0) rotate(20deg)'
+    });
+});
